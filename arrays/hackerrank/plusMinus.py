13c74cf9b668f428eb927b47288ee9ac67d55bf3
Nicholas Carnevale <nickcarnevale2@gmail.com>
2024-07-22 12:19:41 -0400

    added hackerrank

---
+++ arrays/hackerrank/plusMinus.py
@@ -0,0 +1,20 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-plus-minus/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Write your code here
+
+    
