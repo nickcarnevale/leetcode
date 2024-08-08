8255360b946da6956166d07a4927508d8e92963e
Nicholas Carnevale <nickcarnevale2@gmail.com>
2024-08-08 12:18:19 -0400

    solved

--- arrays/35e.py
+++ arrays/35e.py
@@ -9,3 +9,26 @@ class Solution(object):
         """
 
         # Binary Search
+
+        left = 0
+        n = len(nums)
+        right = n-1
+        mid = (left + right) // 2
+
+        if target > nums[-1]:
+            return n
+        elif target < nums[0]:
+            return 0
+
+        while left <= right:
+            
+            mid = (left + right) // 2
+
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
+            
+        return left
