154b482f2f5b74f9e42b1852dddae8e58c07e47b
Nicholas Carnevale <nickcarnevale2@gmail.com>
2024-08-08 12:06:18 -0400

    class

--- arrays/35e.py
+++ arrays/35e.py
@@ -1,2 +1,11 @@
 # https://leetcode.com/problems/search-insert-position/
 
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        # Binary Search
