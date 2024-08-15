d0d3f01c11da53718dea2222a6840e0bca2020e5
Nicholas Carnevale <nickcarnevale2@gmail.com>
2024-08-14 21:43:42 -0400

    merge k sorted linkedlists

---
+++ linkedlist/23h.py
@@ -0,0 +1,55 @@
+# https://leetcode.com/problems/merge-k-sorted-lists/
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+
+        if lists == None or len(lists) == 0:
+            return None
+
+        def MergeTwo(l1, l2):
+            
+            head = ListNode()
+            curr = head
+
+            while l1 and l2: 
+                if l1.val <= l2.val:
+                    curr.next = l1
+                    l1 = l1.next
+                    curr = curr.next
+                else: 
+                    curr.next = l2
+                    l2 = l2.next
+                    curr = curr.next
+
+            if l1:
+                curr.next = l1
+            else:
+                curr.next = l2
+            
+            return head.next
+
+        # had function to merge two sorted lists
+        # merge sort with k sorted lists
+
+        while len(lists) > 1:
+            merged = []
+
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1) in range(len(lists)) else None
+                merged.append(MergeTwo(l1,l2))
+
+            lists = merged
+
+        return lists[0]
+
+        
